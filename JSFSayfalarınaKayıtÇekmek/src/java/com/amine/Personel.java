918f32cb659263b6dbe96352a0df8db98e1dbcd7
yldzamine <yildizamine95@gmail.com>
2018-12-28 20:12:52 +0200

    JSF Kayıt Çekme

---
+++ JSFSayfalarınaKayıtÇekmek/src/java/com/amine/Personel.java
@@ -0,0 +1,62 @@
+package com.amine;
+
+
+public class Personel {
+    public Personel(){
+         }
+    
+    private int personelid;
+    private String personeladi,personelsoyadi;
+    private String personeleposta,personelgorevi,personelmaasi;
+
+    public String getPersoneladi() {
+        return personeladi;
+    }
+
+    public void setPersoneladi(String personeladi) {
+        this.personeladi = personeladi;
+    }
+
+    public String getPersonelsoyadi() {
+        return personelsoyadi;
+    }
+
+    public void setPersonelsoyadi(String personelsoyadi) {
+        this.personelsoyadi = personelsoyadi;
+    }
+
+    public String getPersoneleposta() {
+        return personeleposta;
+    }
+
+    public void setPersoneleposta(String personeleposta) {
+        this.personeleposta = personeleposta;
+    }
+
+    public String getPersonelgorevi() {
+        return personelgorevi;
+    }
+
+    public void setPersonelgorevi(String personelgorevi) {
+        this.personelgorevi = personelgorevi;
+    }
+
+    public String getPersonelmaasi() {
+        return personelmaasi;
+    }
+
+    public void setPersonelmaasi(String personelmaasi) {
+        this.personelmaasi = personelmaasi;
+    }
+
+    public int getPersonelid() {
+        return personelid;
+    }
+
+    public void setPersonelid(int personelid) {
+        this.personelid = personelid;
+    }
+
+   
+  
+}
