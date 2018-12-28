2dc1add05ba94d34e1d922a571d24ec35dab1d50
yldzamine <yildizamine95@gmail.com>
2018-12-28 20:09:18 +0200

    JSF Define

---
+++ JSFDefine/src/java/com/siteadi/Yenii.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siteadi;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Win7
+ */
+@ManagedBean
+@RequestScoped
+public class Yenii {
+
+    /**
+     * Creates a new instance of Yenii
+     */
+    public Yenii() {
+    }
+    
+}
