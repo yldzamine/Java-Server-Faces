5c5c4657c9e155c35527d6fcb7f0ca56ca73068f
yldzamine <yildizamine95@gmail.com>
2018-12-28 19:45:32 +0200

    Resources Bundles
    JSF nin kaynak demetleri

---
+++ Kaynak_Dmetleri/src/java/mesajlar/properties.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mesajlar;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Win7
+ */
+@ManagedBean
+@RequestScoped
+public class properties {
+
+    /**
+     * Creates a new instance of properties
+     */
+    public properties() {
+    }
+    
+}
