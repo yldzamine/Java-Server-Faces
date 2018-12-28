b692d8e9ddf9fc7093f17ea8eacbe0568c9490ea
yldzamine <yildizamine95@gmail.com>
2018-12-28 20:11:38 +0200

    JSF Tag Library

---
+++ JSFOutputStylesheet/web/resources/js/bizim_javascriptimiz.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write('Güzel gören güzel düşünür <br/>'+
+        'Güzel düşünen hayattan lezzet alır.<br/>'+
+        'B.S.N');
