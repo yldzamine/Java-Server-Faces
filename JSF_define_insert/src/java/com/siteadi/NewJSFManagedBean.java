2bd36d4c5afc306a4f03059045db2527ce295165
yldzamine <yildizamine95@gmail.com>
2018-12-28 19:57:48 +0200

    JSF Define_insert

---
+++ JSF_define_insert/src/java/com/siteadi/NewJSFManagedBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siteadi;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Win7
+ */
+@ManagedBean(name="yb")
+@RequestScoped
+public class NewJSFManagedBean {
+
+    public String yonelinecekSayfaGit(){
+        return "sayfa2";
+    }
+    
+}
