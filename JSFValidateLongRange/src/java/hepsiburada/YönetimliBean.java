14456d6b99bd65f5b9062b1cdb3b983eb6303a54
yldzamine <yildizamine95@gmail.com>
2018-12-28 20:17:24 +0200

    JSF Converters

---
+++ JSFValidateLongRange/src/java/hepsiburada/YönetimliBean.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hepsiburada;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Win7
+ */
+@ManagedBean
+@RequestScoped
+public class YönetimliBean {
+
+    /**
+     * Creates a new instance of YönetimliBean
+     */
+    public YönetimliBean() {
+    }
+    int tamsayi;
+
+    public int getTamsayi() {
+        return tamsayi;
+    }
+
+    public void setTamsayi(int tamsayi) {
+        this.tamsayi = tamsayi;
+    }
+}
